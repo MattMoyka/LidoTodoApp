2ee993e7eead31a43730c53f489cd55027dc3e60
mattmoyka <mattmoyka@gmail.com>
2021-12-15 14:36:40 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [list, setList] = useState([])
+  const [temp, setTemp] = useState([])
+  const [add, setAdd] = useState('')
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    setList([...list, add])
+    setAdd('')
+  }
+
+
+  function handleDelete(e) {
+    const templist = list
+    templist.splice(e.target.value, 1)
+    setTemp(templist)
+  }
+
+
+
+  useEffect(() => {
+    setList([...temp])
+  }, [temp])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>Lido Todo App</h1>
+      <form className='todo-form' onSubmit={handleSubmit}>
+        <input type='text' placeholder='Enter item todo' value={add} onChange={(e) => setAdd(e.target.value)} />
+        <button type='submit'>Add</button>
+      </form>
+      <div>
+        {list.map((item, index) => {
+          return (
+
+            <div key={index} className="todo-container">
+              <div >{item}</div>
+              <button value={index} onClick={handleDelete}>Delete</button>
+            </div>
+
+          )
+        })}
+      </div>
     </div>
   );
 }
